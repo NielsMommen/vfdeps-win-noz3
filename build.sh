4e7636efac1d85ff69306c69b5504d01b2abb435
nielsmommen <r0627548@kuleuven.be>
2021-09-14 18:10:30 +0200

    fix msvc installation parameter

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -x # Print what is being executed.
 pwd
 VFDEPS_VERSION=`git describe --always`
 VFDEPS_DIRNAME=vfdeps
+MSVC_INSTALL_DIR=${1:="C:/vfMinVS"}
 
 BUILD_DIR=`pwd`
 mkdir upload
@@ -18,7 +19,6 @@ VFDEPS_DIR=$VFDEPS_PARENT_DIR/$VFDEPS_DIRNAME
 mkdir $VFDEPS_DIR
 /c/cygwin/bin/bash -lc "cd /cygdrive/$BUILD_DIR && make PREFIX=$VFDEPS_DIR"
 
-MSVC_INSTALL_DIR=${MSVC_INSTALL_DIR:="C:/vfMinVS"}
 powershell.exe -ExecutionPolicy RemoteSigned -File build_cxx_deps.ps1 -msvc_install_dir $MSVC_INSTALL_DIR -vfdeps_dir $VFDEPS_DIR
 
 VFDEPS_FILENAME=$VFDEPS_DIRNAME-$VFDEPS_VERSION-$VFDEPS_PLATFORM-noz3.txz
