52772bdeb64fc3708bd792e39776b8d1dfcf81a4
nielsmommen <r0627548@kuleuven.be>
2021-09-16 09:05:45 +0200

    uncomment steps

--- build.sh
+++ build.sh
@@ -9,25 +9,25 @@ VFDEPS_DIRNAME=vfdeps
 MSVC_INSTALL_DIR=${1:-"C:/vfMinVS"}
 
 BUILD_DIR=`pwd`
-#mkdir upload
+mkdir upload
 UPLOAD_DIR=$BUILD_DIR/upload
 
 VFDEPS_PARENT_DIR=C:/
 VFDEPS_PLATFORM=win
 
 VFDEPS_DIR=$VFDEPS_PARENT_DIR/$VFDEPS_DIRNAME
-#mkdir $VFDEPS_DIR
-#/c/cygwin/bin/bash -lc "cd /cygdrive/$BUILD_DIR && make PREFIX=$VFDEPS_DIR"
+mkdir $VFDEPS_DIR
+/c/cygwin/bin/bash -lc "cd /cygdrive/$BUILD_DIR && make PREFIX=$VFDEPS_DIR"
 
 if [[ ! -v GITHUB_ENV ]]; then
     CXX_DEPS_FLAGS="-min_size"
 fi
-#powershell.exe -ExecutionPolicy RemoteSigned -File build_cxx_deps.ps1 -msvc_install_dir "$MSVC_INSTALL_DIR" -vfdeps_dir "$VFDEPS_DIR" $CXX_DEPS_FLAGS
+powershell.exe -ExecutionPolicy RemoteSigned -File build_cxx_deps.ps1 -msvc_install_dir "$MSVC_INSTALL_DIR" -vfdeps_dir "$VFDEPS_DIR" $CXX_DEPS_FLAGS
 
 VFDEPS_FILENAME=$VFDEPS_DIRNAME-$VFDEPS_VERSION-$VFDEPS_PLATFORM-noz3.txz
 VFDEPS_FILEPATH=$UPLOAD_DIR/$VFDEPS_FILENAME
 cd $VFDEPS_PARENT_DIR
-#tar cjf $VFDEPS_FILEPATH $VFDEPS_DIRNAME
+tar cjf $VFDEPS_FILEPATH $VFDEPS_DIRNAME
 cd $BUILD_DIR
 
 ls -l $VFDEPS_FILEPATH
